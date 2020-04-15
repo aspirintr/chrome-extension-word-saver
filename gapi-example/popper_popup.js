a9744d89bbf66e5699edb1a2a3b1e57597469a2d
aspirintr <kasimtasdemir@gmail.com>
2020-04-15 20:10:04 +0300

    A dynamically created form is added. UIkit is integrated for UI

--- gapi-example/popper_popup.js
+++ gapi-example/popper_popup.js
@@ -5,6 +5,7 @@ tooltip.innerHTML = "My tooltip <div id=\"arrow\" data-popper-arrow></div>";
     adaasd hhg<div x-arrow></div>\
   </div>";*/
 tooltip.id = "tooltip";
+createForm(tooltip);
 
 document.body.appendChild(tooltip);
 
@@ -82,6 +83,72 @@ addEventListener("scroll", function () {
     if(popperInstance)
       updateAndShow();
 });
+
+///////////
+
+function createForm(tooltipElement) {
+  var main_container_div = document.createElement("div");
+  main_container_div.setAttribute("class", "uk-container-xsmall uk-width-medium");
+  
+  var wordTextbox_saveButton_div = document.createElement("div");
+  wordTextbox_saveButton_div.setAttribute("class", "uk-margin-small uk-grid-small uk-grid"); 
+  wordTextbox_saveButton_div.setAttribute("uk-grid", ""); 
+  
+  var wordTextbox_div = document.createElement("div");
+  wordTextbox_div.setAttribute("class", "uk-width-expand uk-first-column"); 
+  
+  var saveButton_div = document.createElement("div");
+  saveButton_div.setAttribute("class", "uk-width-auto"); 
+
+  var sentenceTextbox_div = document.createElement("div");
+  sentenceTextbox_div.setAttribute("class", "uk-margin-small");
+  
+  var wordTextbox_input = document.createElement("input");
+  //wordTextbox_input.setAttribute("autofocus", ""); 
+  wordTextbox_input.setAttribute("class", "uk-input"); 
+  wordTextbox_input.setAttribute("type", "text"); 
+  wordTextbox_input.setAttribute("placeholder", "enter a word"); 
+  wordTextbox_input.setAttribute("value", "word");  
+  
+  var sentenceTextbox_input = document.createElement("input");
+  sentenceTextbox_input.setAttribute("class", "uk-input"); 
+  sentenceTextbox_input.setAttribute("type", "text"); 
+  sentenceTextbox_input.setAttribute("placeholder", "enter a sentence"); 
+  sentenceTextbox_input.setAttribute("value", "sentence"); 
+  
+  var saveButton_button = document.createElement("button");
+  saveButton_button.setAttribute("class", "uk-button uk-button-default uk-width-1-1"); 
+  saveButton_button.innerHTML = "Save";
+  
+  main_container_div.appendChild(wordTextbox_saveButton_div);
+  main_container_div.appendChild(sentenceTextbox_div);
+  
+  wordTextbox_saveButton_div.appendChild(wordTextbox_div);
+  wordTextbox_saveButton_div.appendChild(saveButton_div);
+  wordTextbox_div.appendChild(wordTextbox_input);
+  saveButton_div.appendChild(saveButton_button);
+  
+  sentenceTextbox_div.appendChild(sentenceTextbox_input);
+  
+  
+  /*
+  main_container_div
+  { //structure
+    wordTextbox_saveButton_div
+      wordTextbox_div
+        wordTextbox_input
+      saveButton_div
+        saveButton_button
+        
+    sentenceTextbox_div
+      sentenceTextbox_input
+      
+  }
+  */
+  tooltipElement.appendChild(main_container_div);
+};
+
+
 
 /*
 class RangeRef {
