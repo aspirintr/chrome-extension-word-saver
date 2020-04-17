4f853fa3171e056def4d699193eb35e72c4cdf48
aspirintr <kasimtasdemir@gmail.com>
2020-04-18 00:05:25 +0300

    can save word and sentence. doesn't work at play.books.google

--- gapi-example/popper_popup.js
+++ gapi-example/popper_popup.js
@@ -78,8 +78,116 @@ class PopupForm {
   }
 }
 ///////////
+var iframe;
+function getSelectedSentence(){
+  if(typeof iframe === 'undefined'){ //if not defined
+    iframe = document.createElement('iframe');
+    iframe.style.display = "none"; //"block";
+
+    document.body.appendChild(iframe);
+  }
+
+  sel = window.getSelection();
+  if (!sel.isCollapsed) {
+      var bd = document.body.cloneNode(true);
+      iframe.contentDocument.body.innerHTML = bd.innerHTML;
+      ibd = iframe.contentDocument.body;
+      /*
+      "..tiam.." is selected from the second paragraph
+      sel.anchorNode.parentNode.parentNode.parentNode.nodeName
+      > "BODY"
+      Array.from(sel.anchorNode.parentNode.childNodes).indexOf(sel.anchorNode)
+      0
+      Array.from(sel.anchorNode.parentNode.parentNode.childNodes).indexOf(sel.anchorNode.parentNode)
+      7
+      Array.from(sel.anchorNode.parentNode.parentNode.parentNode.childNodes).indexOf(sel.anchorNode.parentNode.parentNode)
+      3
+      sel.anchorOffset
+      1
+      sel.focusOffset
+      4
+
+      startNodeTreeIndexListFromBody = {3,7,0}
+      startNode == bd.childNodes[3].childNodes[7].childNodes[0]
+      startOffset = 1
+      endNode == bd.childNodes[3].childNodes[7].childNodes[0]
+      endOffset = 4
+      */
+      pNode = sel.anchorNode;
+      var arrStartNodeTreeIndexListFromBody = [];
+      while(pNode.nodeName.toUpperCase() != "BODY"){
+          i = Array.from(pNode.parentNode.childNodes).indexOf(pNode);
+          arrStartNodeTreeIndexListFromBody.push(i);
+          pNode = pNode.parentNode;
+      }
+
+      pNode = sel.focusNode;
+      var arrEndNodeTreeIndexListFromBody = [];
+      while(pNode.nodeName.toUpperCase() != "BODY"){
+          i = Array.from(pNode.parentNode.childNodes).indexOf(pNode);
+          arrEndNodeTreeIndexListFromBody.push(i);
+          pNode = pNode.parentNode;
+      }
+
+      var arr = [];
+      cNode = ibd;
+      arr = arrStartNodeTreeIndexListFromBody;
+      while (arr.length>0){
+          cNode = cNode.childNodes[arr.pop()];
+      }
+      var startNode =  cNode; //ibd.childNodes[3].childNodes[7].childNodes[0];
+
+      cNode = ibd;
+      arr = arrEndNodeTreeIndexListFromBody;
+      while (arr.length>0){
+          cNode = cNode.childNodes[arr.pop()];
+      }        
+      var endNode = cNode; // ibd.childNodes[3].childNodes[7].childNodes[0];
+
+      var startOffset = sel.anchorOffset;
+      var endOffset = sel.focusOffset;
+
+      isel = iframe.contentWindow.getSelection();
+      isel.removeAllRanges();
+      irg = iframe.contentDocument.createRange();
+      irg.setStart(startNode, startOffset);
+      irg.setEnd(endNode, endOffset);
+
+      isel.addRange(irg);
 
 
+      irg2 = iframe.contentDocument.createRange();
+      irg2.setStart(isel.anchorNode, isel.anchorOffset);
+      irg2.setEnd(isel.focusNode, isel.focusOffset);
+      var backwards = irg2.collapsed;
+      irg2.detach();
+
+      // modify() works on the focus of the selection
+      var endNode2 = isel.focusNode, endOffset2 = isel.focusOffset;
+      isel.collapse(isel.anchorNode, isel.anchorOffset);
+
+      var direction = [];
+      if (backwards) {
+      direction = ['backward', 'forward'];
+      } else {
+      direction = ['forward', 'backward'];
+      }
+
+      isel.modify("move", direction[0], "character");
+      isel.modify("move", direction[1], "sentence");
+      isel.extend(endNode2, endOffset2);
+      isel.modify("extend", direction[1], "character");
+      isel.modify("extend", direction[0], "sentence");
+
+      return isel.toString();
+  }
+  return "";
+}
+
+    
+///////////
+var sentence = "";
+var selectionDragging = true;
 var tooltip = document.createElement("div");
 tooltip.innerHTML = "<div id=\"arrow\" data-popper-arrow></div>";
 /*tooltip.innerHTML = " <div class=\"popper\">\
@@ -88,11 +196,10 @@ tooltip.innerHTML = "<div id=\"arrow\" data-popper-arrow></div>";
 tooltip.id = "tooltip";
 //createForm(tooltip);
 let popupForm = new PopupForm(tooltip);
-
 document.body.appendChild(tooltip);
 
-var sel = window.getSelection();
 
+var sel = window.getSelection();
 function generateGetBoundingClientRect(w = 0, h = 0, t = 0 , r = 0 , b = 0 , l = 0) {
   return () => ({
     width: w,
@@ -143,6 +250,7 @@ function hide() {
 function updateAndShow() {
     if (!sel.isCollapsed) {
       popupForm.wordTextbox_input.value = sel.toString();
+      popupForm.sentenceTextbox_input.value  = sentence;
 
 
       var r = sel.getRangeAt(0).getBoundingClientRect();
@@ -153,15 +261,60 @@ function updateAndShow() {
       popperInstance.update();
     }
 };
-window.addEventListener('mouseup', updateAndShow);
+/*window.addEventListener('mouseup', function (){
+  sel = window.getSelection();
+  if(!sel.collapsed){
+    sentence = getSelectedSentence();
+    updateAndShow();
+  }
+  
+});*/
+
+/*
+window.addEventListener('mouseup', function (){
+  selectionDragging = false;
+});
+window.addEventListener('mousedown', function () {
+  selectionDragging = true;
+});
+document.addEventListener('selectionchange', function(){
+  if(!selectionDragging){
+    sel = window.getSelection();
+    if(!sel.collapsed){
+      sentence = getSelectedSentence();
+      updateAndShow();
+    }
+  }
+  
+});
+document.addEventListener("dragstart", function(event) {
+  selectionDragging = true;
+});
+
+document.addEventListener("drag", function(event) {
+  selectionDragging = true;
+});
+
+document.addEventListener("dragend", function(event) {
+  selectionDragging = false;
+});
+*/
 /*window.addEventListener('mousedown', function () {
     hide();
 });*/
+document.addEventListener('mouseup', function(){
+  sel = window.getSelection();
+  if(!sel.collapsed){
+    sentence = getSelectedSentence();
+    updateAndShow();
+  }
+});
 window.
 addEventListener("scroll", function () {
     if(popperInstance)
       updateAndShow();
 });
+
 document.
 scrollingElement.
 addEventListener("scroll", function () {
